7e3281209bb7e929a1a9a092032ed28b106521b0
Armando Escalier <oneliner.rb@outlook.com>
2025-06-23 20:14:42 -0600

    Run rubocop in PigLatin challenge

--- ruby/medium/pig_latin/pig_latin.rb
+++ ruby/medium/pig_latin/pig_latin.rb
@@ -1,24 +1,23 @@
+# frozen_string_literal: true
+
 class PigLatin
   def self.translate(word)
     @word = word
     rules
   end
 
-  private
-
   def self.rules
-    vowels = ["a","e","i","o","u"]
-    rule_1 = ["xr","yt"]
+    vowels = %w[a e i o u]
+    rule_1 = %w[xr yt]
 
-    case
     # start with vocal or "xr" or "yt"
-    when vowels.any? { |v| @word.start_with?(v) } || rule_1.any? { |r| @word.start_with?(r) }
+    if vowels.any? { |v| @word.start_with?(v) } || rule_1.any? { |r| @word.start_with?(r) }
       rule1
-    when !vowels.any? { |v| @word.start_with?(v) } && @word.include?("qu")
+    elsif vowels.none? { |v| @word.start_with?(v) } && @word.include?('qu')
       rule3
-    when !vowels.any? { |v| @word.start_with?(v) } && @word.include?("y")
+    elsif vowels.none? { |v| @word.start_with?(v) } && @word.include?('y')
       rule4
-    when !vowels.any? { |v| @word.start_with?(v) }
+    elsif vowels.none? { |v| @word.start_with?(v) }
       rule2
     end
   end
@@ -28,7 +27,7 @@ class PigLatin
   end
 
   def self.rule2
-    vowels = ["a","e","i","o","u"]
+    vowels = %w[a e i o u]
 
     index_of_first_vowel = @word.chars.index { |char| vowels.include?(char) }
     consonant_cluster = @word[0...index_of_first_vowel]
@@ -38,12 +37,12 @@ class PigLatin
   end
 
   def self.rule3
-    array = @word.split("qu")
+    array = @word.split('qu')
     @word = "#{array[1]}#{array[0]}quay"
   end
 
   def self.rule4
-    array = @word.split("y")
+    array = @word.split('y')
     @word = "y#{array[1]}#{array[0]}ay"
   end
 end
